e6b91faf555c5802bbd5e104903af343850d79ea
Markus Ullmann <jokey@jokeybook.fritz.box>
2009-11-25 01:10:02 +0100

    Shoutbox should remember where user come from and redirect back after posting

--- pyClanSphere/plugins/shoutbox/views.py
+++ pyClanSphere/plugins/shoutbox/views.py
@@ -8,11 +8,13 @@
     :copyright: (c) 2009 by the pyClanSphere Team, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
+from werkzeug import escape
 from werkzeug.exceptions import NotFound
 
-from pyClanSphere.api import db
+from pyClanSphere.api import db, url_for, get_request
 from pyClanSphere.application import render_response
 from pyClanSphere.privileges import assert_privilege
+from pyClanSphere.utils.http import get_redirect_target
 from pyClanSphere.widgets import Widget
 
 from pyClanSphere.plugins.shoutbox.forms import ShoutboxEntryForm, DeleteShoutboxEntryForm
@@ -32,7 +34,7 @@ class ShoutboxWidget(Widget):
         self.hide_form = hide_form
         self.entries = ShoutboxEntry.query.order_by(ShoutboxEntry.postdate.desc()) \
                                     .limit(entrycount).all()
-
+        self.newposturl = escape(url_for('shoutbox/post', next=get_request().path))
 
 def make_shoutbox_entry(request):
     form = ShoutboxEntryForm()
@@ -45,7 +47,8 @@ def make_shoutbox_entry(request):
             db.commit()
             # as this affects pretty much all visible pages, we flush cache here
             request.app.cache.clear()
-            return form.redirect('core/index')
+            target = get_redirect_target()
+            return form.redirect(target if target is not None else 'core/index')
 
     return render_response('shoutbox_post.html', form=form.as_widget(),
                            widgetoptions=['hide_shoutbox_note'])
