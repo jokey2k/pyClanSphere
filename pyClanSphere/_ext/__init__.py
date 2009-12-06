e6b98218c9d184866b75aab10d624a3f58f0de2a
Markus Ullmann <jokey@jokeybook.fritz.box>
2009-12-06 14:41:06 +0100

    BBCode support for news entries

--- pyClanSphere/_ext/__init__.py
+++ pyClanSphere/_ext/__init__.py
@@ -13,6 +13,11 @@
     Small enough to not put it up as separate package, used to parse Trackmania's
     Challenge Files as war maps.
 
+    PostMarkup
+    ----------
+    Class to sanely render BBCode or Post Markup how the author calls it.
+    It's thankyou ware, so please remember to say thank you there :)
+
     :copyright: (c) 2009 by the pyClanSphere Team, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
