94cb57d93afaa0cc195d043af2b03bb20e847712
Markus Ullmann <mail@markus-ullmann.de>
2010-04-11 23:37:58 +0200

    remove some unused sqlalchemy links

--- pyClanSphere/database.py
+++ pyClanSphere/database.py
@@ -276,8 +276,6 @@ db.association_proxy = association_proxy
 db.attribute_loaded = attribute_loaded
 db.AttributeExtension = AttributeExtension
 db.AutoAddExtension = AutoAddExt
-db.EXT_CONTINUE = orm.EXT_CONTINUE
-db.EXT_STOP = orm.EXT_STOP
 db.attribute_mapped_collection = attribute_mapped_collection
 
 #: called at the end of a request
