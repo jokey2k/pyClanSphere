724963d21b306058adbc70398c19eab6d1a165bd
Markus Ullmann <mail@markus-ullmann.de>
2010-05-10 14:00:40 +0200

    drop draft-first check, hardly makes sense...

--- pyClanSphere/plugins/news/forms.py
+++ pyClanSphere/plugins/news/forms.py
@@ -53,9 +53,6 @@ class NewsForm(forms.Form):
     def validate_status(self, status):
         """Users without NEWS_PUBLIC are not allowed to switch status flag"""
 
-        if not self.news and status == STATUS_PUBLISHED:
-            raise ValidationError(_(u'Initially news should always be drafts'))
-
         if status == STATUS_PUBLISHED and not \
                 self.news.can_publish():
             raise ValidationError(_(u'You have no permission to publish posts.'))
